b606a5cb81dee403c3802d640919d0fabd55f6e5
bkljuno <benjaminkljn@protonmail.ch>
2024-11-11 19:19:31 +0100

    Initial commit

--- main.py
+++ main.py
@@ -45,28 +45,28 @@ def test_complex_trajectories(
     robot.set_position()
     print("\nExecuting Star Pattern - Constant Speed:")
     pattern_movement_controller.execute_star_pattern(
-        radius=1.0, points=5, speed=15.0, dynamics=MovementDynamics.CONSTANT
+        radius=1.0, points=5, speed=10.0, dynamics=MovementDynamics.CONSTANT
     )
     robot.set_position()
     print("\nExecuting Zigzag Pattern - Constant Speed:")
     pattern_movement_controller.execute_zigzag_pattern(
-        length=2.0,
+        length=1.0,
         width=1.0,
-        num_zigzags=2,
+        num_zigzags=4,
         speed=10.0,
         dynamics=MovementDynamics.CONSTANT,
     )
     robot.set_position()
     print("\nExecuting Figure-Eight Pattern - Constant Speed:")
     pattern_movement_controller.execute_figure_eight_pattern(
-        radius=1.0, speed=0.5, dynamics=MovementDynamics.CONSTANT
+        radius=0.5, speed=0.5, dynamics=MovementDynamics.CONSTANT
     )
     robot.set_position()
     print("\nExecuting Spiral Pattern - Constant Speed:")
     pattern_movement_controller.execute_spiral_pattern(
         start_radius=0.5,
         end_radius=2.0,
-        speed=2.0,
+        speed=0.5,
         num_revolutions=2,
         dynamics=MovementDynamics.CONSTANT,
     )
@@ -79,8 +79,8 @@ def main():
         robot.vrep_connection, robot.wheel_joints
     )
     pattern_movement_controller = PatternMovementController(wheel_movement_controller)
-    # test_movement_patterns(pattern_movement_controller)
-    # test_different_velocities(pattern_movement_controller)
+    test_movement_patterns(pattern_movement_controller)
+    test_different_velocities(pattern_movement_controller)
     test_complex_trajectories(robot, pattern_movement_controller)
 
 
