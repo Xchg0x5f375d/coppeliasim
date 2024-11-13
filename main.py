43ec96bef9fb486479a8cd7e3b795f1ef668c35a
bkljuno <benjaminkljn@protonmail.ch>
2024-11-14 00:13:07 +0100

    .

--- main.py
+++
@@ -1,8 +0,0 @@
-def main():
-    raise Exception(
-        "This file is intentionally left empty. Please checkout to specific exercise branch (Exercise-2, Exercise-3, etc.) to access the actual implementation."
-    )
-
-
-if __name__ == "__main__":
-    main()
